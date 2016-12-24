91d64ee33f807060c2a70db015e049e6f9f2b41d
Marciel Leal <marcielmanoel15@gmail.com>
2016-12-24 09:36:17 -0300

    OK

---
+++ uri1050.rb
@@ -0,0 +1,14 @@
+lista={ 61=> "Brasilia",
+    71=>"Salvador",
+    11=>"Sao Paulo",
+    21=>"Rio de Janeiro",
+    32=>"Juiz de Fora",
+    19=>"Campinas",
+    27=>"Vitoria",
+    31=>"Belo Horizonte"
+}
+
+ddd=gets.to_i
+if lista[ddd] then puts lista[ddd]
+else puts "DDD nao cadastrado"
+end
